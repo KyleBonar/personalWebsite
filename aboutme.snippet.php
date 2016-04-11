ad5966f619705da120fa5f52481110bea59c3866
Kyle Bonar <Kyle@dhcp-10-202-108-249.tamulink.tamu.edu>
2016-04-11 15:50:08 -0500

    Adding files for first time.

---
+++ aboutme.snippet.php
@@ -0,0 +1,16 @@
+
+<div id='about-background' >
+    <div class='wrap' >
+        <div class='row' >
+            <div class='columns' >
+                <img id='about-image' src='/images/FullSizeRender.jpg' />
+                <br />
+            </div>
+        </div>
+        <div class='row align-center' >
+            <div class='medium-10 columns' >
+                <p class='centered'>My name is Kyle Bonar. I am an web developer, and in May 2016, I will graduate from Texas A&M University with a BS in physics!</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ examples.snippet.php
@@ -0,0 +1,247 @@
+
+
+<br />
+<br />
+
+<div id='examples-background' >
+	<div class='wrap'>
+		<div class='row'>
+			<div class='medium-12 columns' >
+				<h3>Portfolio</h3>
+				<hr />
+			</div>
+		</div>	
+		
+
+		<div class='hide-for-small-only'>
+			<div class='row' >
+				<div class='medium-4 large-4 columns'>
+					<a data-open='portfolio-modal-1'><img class='portfolio-image-reg' src='/images/Personal_Page.png' ></a>
+				</div>
+				<div class='medium-4 columns'>
+					<a data-open='portfolio-modal-2'><img class='portfolio-image-reg' src='/images/events_cal.png'></a>
+				</div>
+				<div class='medium-4 columns'>
+					<a data-open='portfolio-modal-3'><img class='portfolio-image-reg' src='/images/REU.png'></a>
+				</div>
+			</div>
+			<br>
+			<div class='row' >
+				<div class='medium-4 large-4 columns'>
+					<a data-open='portfolio-modal-4'><img class='portfolio-image-reg' src='/images/event-pages.png' ></a>
+				</div>
+				<div class='medium-4 columns'>
+					<a data-open='portfolio-modal-5'><img class='portfolio-image-reg' src='/images/Directory.png'></a>
+				</div>
+				<div class='medium-4 columns'>
+					<a data-open='portfolio-modal-6'><img class='portfolio-image-reg' src='/images/news-form.png'></a>
+				</div>
+			</div>
+		</div>
+
+		<div class='show-for-small-only' >
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-1'><img class='portfolio-image-small' src='/images/Personal_Page.png'></a>
+				</div>
+			</div>
+			
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-2'><img class='portfolio-image-small' src='/images/events_cal.png'></a>
+				</div>
+			</div>
+			
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-3'><img class='portfolio-image-small' src='/images/REU.png'></a>
+				</div>
+			</div>
+			
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-4'><img class='portfolio-image-small' src='/images/event-pages.png'></a>
+				</div>
+			</div>
+			
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-5'><img class='portfolio-image-small' src='/images/Directory.png'></a>
+				</div>
+			</div>
+			
+			<div class='row' >
+				<div class='columns'>
+					<a data-open='portfolio-modal-6'><img class='portfolio-image-small' src='/images/news-form.png'></a>
+				</div>
+			</div>
+			<br>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="reveal" id="portfolio-modal-1" data-reveal>
+
+	<h3 class='modal-title'>Personal Pages</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>Faculty request pages that were customizable and could show what they wanted.</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="reveal" id="portfolio-modal-2" data-reveal>
+
+	<h3 class='modal-title'>Event Calendar</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/events_cal.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/events_cal.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>To get away from Google Calendar and maintain an in-house calendar which showed all of the events that are going on for the given month.</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating a multidimensional array of events for the month, which is created from a MySQL database, and then comparing to the day of the month as the calendar is being built. Database queries are only executed for the visible month as to not bloat the page. Specific members are given access to forms which allow events to be created and removed, creating calendar that is always up to date. </p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="reveal" id="portfolio-modal-3" data-reveal>
+
+	<h3 class='modal-title'>Personal Pages</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>Faculty request pages that were customizable and could show what they wanted</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="reveal" id="portfolio-modal-4" data-reveal>
+
+	<h3 class='modal-title'>Personal Pages</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>Faculty request pages that were customizable and could show what they wanted</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="reveal" id="portfolio-modal-5" data-reveal>
+
+	<h3 class='modal-title'>Personal Pages</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>Faculty request pages that were customizable and could show what they wanted</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="reveal" id="portfolio-modal-6" data-reveal>
+
+	<h3 class='modal-title'>Personal Pages</h3>
+	<hr />
+	<div class='hide-for-small-only'>
+		<div class='row' >
+			<div class='columns'>
+				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+	<div class='show-for-small-only' >
+		<div class='row' >
+			<div class='columns'>
+				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+			</div>
+		</div>
+	</div>
+  <p class="lead">Purpose:</p>
+  <p>Faculty request pages that were customizable and could show what they wanted</p>
+  <p class='lead'> Acheived By:</p>
+  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+
