041960ee43c9d3a8f9bd319206016a0c46c3a57c
Kyle Bonar <kylewb2011@gmail.com>
2016-04-12 15:46:44 -0500

    Removed one portfolio div so only 5 appear. Images are still too large, will need to resize them to improve load time.

--- examples.snippet.php
+++ examples.snippet.php
@@ -4,7 +4,7 @@
 <br />
 
 <div id='examples-background' >
-	<div class='wrap'>
+	<div class='wrap' id='portfolio'>
 		<div class='row'>
 			<div class='medium-12 columns' >
 				<h3>Portfolio</h3>
@@ -26,15 +26,12 @@
 				</div>
 			</div>
 			<br>
-			<div class='row' >
+			<div class='row align-center' >
 				<div class='medium-4 large-4 columns'>
 					<a data-open='portfolio-modal-4'><img class='portfolio-image-reg' src='/images/event-pages.png' ></a>
 				</div>
-				<div class='medium-4 columns'>
+				<div class='medium-4 columns end'>
 					<a data-open='portfolio-modal-5'><img class='portfolio-image-reg' src='/images/Directory.png'></a>
-				</div>
-				<div class='medium-4 columns'>
-					<a data-open='portfolio-modal-6'><img class='portfolio-image-reg' src='/images/news-form.png'></a>
 				</div>
 			</div>
 		</div>
@@ -70,11 +67,6 @@
 				</div>
 			</div>
 			
-			<div class='row' >
-				<div class='columns'>
-					<a data-open='portfolio-modal-6'><img class='portfolio-image-small' src='/images/news-form.png'></a>
-				</div>
-			</div>
 			<br>
 		</div>
 
@@ -192,56 +184,31 @@
 
 <div class="reveal" id="portfolio-modal-5" data-reveal>
 
-	<h3 class='modal-title'>Personal Pages</h3>
+	<h3 class='modal-title'>Directory List</h3>
 	<hr />
 	<div class='hide-for-small-only'>
 		<div class='row' >
 			<div class='columns'>
-				<img class='modal-image-reg' src='/images/Personal_Page.png' >
+				<img class='modal-image-reg' src='/images/Directory.png' >
 			</div>
 		</div>
 	</div>
 	<div class='show-for-small-only' >
 		<div class='row' >
 			<div class='columns'>
-				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
+				<img class='portfolio-image-small' src='/images/Directory.png' >
 			</div>
 		</div>
 	</div>
   <p class="lead">Purpose:</p>
-  <p>Faculty request pages that were customizable and could show what they wanted</p>
+  <p>To create a maintainable, and complete, list which shows all of the active faculty, staff, graduate, and researchers. </p>
   <p class='lead'> Acheived By:</p>
-  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
+  <p>Dynamically pulling appropriate rows from a MySQL database based on the "Filter By" drop downs. Creating forms for select members to add/modify/remove other members from the database allow for the directory list to always be up to date.</p> 
   <button class="close-button" data-close aria-label="Close modal" type="button">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
 
-<div class="reveal" id="portfolio-modal-6" data-reveal>
 
-	<h3 class='modal-title'>Personal Pages</h3>
-	<hr />
-	<div class='hide-for-small-only'>
-		<div class='row' >
-			<div class='columns'>
-				<img class='modal-image-reg' src='/images/Personal_Page.png' >
-			</div>
-		</div>
-	</div>
-	<div class='show-for-small-only' >
-		<div class='row' >
-			<div class='columns'>
-				<img class='portfolio-image-small' src='/images/Personal_Page.png' >
-			</div>
-		</div>
-	</div>
-  <p class="lead">Purpose:</p>
-  <p>Faculty request pages that were customizable and could show what they wanted</p>
-  <p class='lead'> Acheived By:</p>
-  <p>Creating several specialized forms which allowed Faculty to modify, add, or delete information within a MySQL database. The resulting database would then be acessed and it's information shown in an appealing manner.</p> 
-  <button class="close-button" data-close aria-label="Close modal" type="button">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
 
 
