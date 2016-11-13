0bb1d508bcc7adb5043c6e0a2cac42199bfd5c31
Kyle Bonar <kylewb2011@gmail.com>
2016-11-13 17:54:45 -0600

    Added description for Physics website, added icon for linking out, reformatted portfolio page, added slight styling

--- index.php
+++ index.php
@@ -18,17 +18,29 @@
 			<span class='header'>Work</span>
 			<p class='details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor nibh eget nisi posuere molestie. Aenean volutpat vel ante in lacinia. Praesent dapibus lacinia elit at placerat. Aenean sollicitudin nibh sed faucibus volutpat. Curabitur gravida nunc rhoncus tempus dignissim. In eu elementum diam. Aenean nisi mi, faucibus ut erat quis, aliquet posuere nunc. </p>
 			
-			<span class='header'>Physics Department at Texas A&M</span>
-			<p class='details'><span class='descriptor'>Purpose: </span> 
-			<br><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery </p>
+			<div class='info-block'>
+				<a href='https://physics.tamu.edu'><span class='header'>Physics Department at Texas A&M</span><img class='external' src='http://kylebonar.com/images/external-link.svg'></a>
+				<div class='details'>
+					<p><span class='descriptor'>Purpose: </span> In just two short months, the Physics Department website was given a lot of love by receiving a new server host, an updated backend, a much more appealing style, and a plethera of options for staff and faculty members to keep the content updated. This project was completed within a very small team and is the project I currently spend most of my time maintaining and adding additional functionality to.</p>
+					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery, AJAX </p>
+				</div>
+			</div>
 
-			<span class='header'>Mitchell Institute at Texas A&M</span>
-			<p class='details'><span class='descriptor'>Purpose: </span> As a student Web Development Assistant, I worked on a small team to completely redesign the home domain for the Astronomy Department. We were responsible for migrating years of static content into a MySQL database, for both easier managing and record keeping, while also ensuring that all pages become responsive. Several custom Wordpress themes were developed for this project and remain actively maintained today.
-			<br><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery</p>
+			<div class='info-block'>
+				<a href='http://mitchell.tamu.edu'><span class='header'>Mitchell Institute at Texas A&M</span><img class='external' src='http://kylebonar.com/images/external-link.svg'></a>
+				<div class='details'>
+					<p><span class='descriptor'>Purpose: </span> As a student Web Development Assistant, I worked on a small team to recreate the Astronomy Department's home domain. We were responsible for migrating years of static content into a MySQL database, for both easier managment and record keeping, while also ensuring that every page be responsive. Several custom Wordpress themes were developed for this project and are actively maintained today.</p>
+					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery, AJAX</p>
+				</div>
+			</div>
 
-			<span class='header'>JavaScript Fun Quiz</span>
-			<p class='details'><span class='descriptor'>Purpose: </span>You can find some of the public code I've written at <a href='https://github.com/KyleBonar'>https://github.com/KyleBonar</a>.
-			<br><span class='descriptor'>Tools: </span> JavaScript, HTML5, CSS3</p>
+			<div class='info-block'>
+				<a href=''><span class='header'>JavaScript Fun Quiz</span><img class='external' src='http://kylebonar.com/images/external-link.svg'></a>
+				<div class='details'>
+					<p><span class='descriptor'>Purpose: </span>Howdy</p>
+					<p><span class='descriptor'>Tools: </span> JavaScript, HTML5, CSS3</p>
+				</div>
+			</div>
 			
 
 		</div>
@@ -36,10 +48,14 @@
 			<span class='header'>GitHub</span>
 			<p class='details'>You can find some of the public code I've written at <a href='https://github.com/KyleBonar'>https://github.com/KyleBonar</a>.</p>
 			<span class='header'>Email</span>	
-			<p class='details'>Email me at <a href='mailto:kylewb2011@gmail.com'>kylewb2011@gmail.com</a>. I'll be sure to get back to you as soon as I can.</p>
+			<p class='details'>Email me at <a href='mailto:kylewbonar@gmail.com'>kylewbonar@gmail.com</a>. I'll be sure to get back to you as soon as I can.</p>
 		</div>
 		<div id='resume-block' class='page'>
-
+			<span class='header'>JavaScript Fun Quiz</span>
+			<div class='details'>
+				<p><span class='descriptor'>Purpose: </span>Howdy</p>
+				<p><span class='descriptor'>Tools: </span> JavaScript, HTML5, CSS3</p>
+			</div>
 		</div>
 
 	</div>
