8b58260759351c6eb37f7023663056e2bdff0e30
Kyle Bonar <kylewb2011@gmail.com>
2016-11-21 08:20:50 -0600

    Added responsiveness. Changed path locations. Updated content

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <html>
 <head>
 <link href="https://fonts.googleapis.com/css?family=Rubik:300i,500" rel="stylesheet"> 
-<link rel="stylesheet" type="text/css" href="http://kylebonar.com/testerino/style.css">
+<link rel="stylesheet" type="text/css" href="http://kylebonar.com/style.css">
+<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
 </head>
 
 
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include 'header.php'; ?>
 
+
 <div id='content-wrapper'>
 	<div id='navigation'>
 		<a href='#' class='img-link active' id='home'><img src='http://kylebonar.com/images/home.svg'><span>Home</span></a>
@@ -10,9 +11,9 @@
 
 	<div id='main'>
 		<div id='home-block' class='page active'>
-			<img src='http://kylebonar.com/images/KBonar.jpg'>
+			<img alt='Kyle Bonar image' src='http://kylebonar.com/images/KBonar.jpg'>
 			<span id='name'>Kyle Bonar</span>
-			<span id='self-title'>Self Proclaimed Awesome Guy</span>
+			<span id='self-title'>Web Developer and IT Specialist</span>
 		</div>
 		<div id='portfolio-block' class='page'>
 			
@@ -25,8 +26,8 @@
 					</svg>
 				</a>
 				<div class='details'>
-					<p><span class='descriptor'>Purpose: </span> In just two short months, the Physics Department website was given a lot of love by receiving a new server host, an updated backend, a much more appealing style, and a plethera of options for staff and faculty members to keep the content updated. This project was completed within a very small team and is the project I currently spend most of my time maintaining and adding additional functionality to.</p>
-					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery, AJAX </p>
+					<p><span class='descriptor'>Purpose: </span> In just two short months, the Physics Department website was given a lot of love by receiving a new server host, an updated backend, a new responsive design, and a plethera of options for staff and faculty members to keep the content updated. This project was completed within a very small team and is the project I currently spend most of my time maintaining and adding additional functionality to.</p>
+					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, Foundation 6, HTML5, MySQL, Wordpress, JavaScript, AJAX </p>
 				</div>
 			</div>
 
@@ -39,12 +40,12 @@
 				</a>
 				<div class='details'>
 					<p><span class='descriptor'>Purpose: </span> As a student Web Development Assistant, I worked on a small team to recreate the Astronomy Department's home domain. We were responsible for migrating years of static content into a MySQL database, for both easier managment and record keeping, while also ensuring that every page be responsive. Several custom Wordpress themes were developed for this project and are actively maintained today.</p>
-					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery, AJAX</p>
+					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, Foundation 6, HTML5, MySQL, Wordpress, JavaScript, AJAX</p>
 				</div>
 			</div>
 
 			<div class='info-block'>
-				<a href='http://imgur.com/9KLEPn2' target='_blank'><span class='header'>Physics and Astronomy Information Portal </span>
+				<a href='http://imgur.com/9KLEPn2' target='_blank'><span class='header'>Department Information Portal </span>
 				<svg vector-effect="non-scaling-stroke" xmlns="http://www.w3.org/2000/svg" width="29" height="32px" viewBox="0 0 1289 1181">
 						<rect fill="none" x="-1" y="-1" width="1289" height="1181"/>
 						<path d="m1010.4 714.2l0 244q0 91.3-60.5 156.1t-145.7 64.8l-595.9 0q-85.2 0-145.7-64.8t-60.5-156.1l0-638.1q0-91.3 60.5-156.1t145.7-64.8l504.2 0q10 0 16.5 6.9t6.4 17.6l0 49.1q0 10.7-6.4 17.6t-16.5 6.9l-504.2 0q-47.3 0-80.9 36t-33.7 86.7l0 638.1q0 50.6 33.7 86.7t80.9 36l595.9 0q47.3 0 80.9-36t33.7-86.7l0-245.4q0-10.7 6.4-17.6t16.5-6.9l45.8 0q10 0 16.5 6.9t6.4 17.9zm275-662.9l0 392.7q0 19.9-13.6 34.5t-32.2 14.6 -32.2-14.6l-126.1-135 -467 500q-7.2 7.7-16.5 7.7t-16.5-7.7l-81.6-87.4q-7.2-7.7-7.2-17.6t7.2-17.6l467-500 -126.1-135q-13.6-14.6-13.6-34.5t13.6-34.5 32.2-14.6l366.7 0q18.6 0 32.2 14.6t13.6 34.5z" fill="#000"/>
@@ -52,25 +53,29 @@
 				</a>
 				<div class='details'>
 					<p><span class='descriptor'>Purpose: </span>Create a one-stop-shop for faculty, researchers, graduate students, and staff members to modify any appropriate information on their respective department websites. Members can upload news, submit events to our calendars and tvs, change their research team, modify their publications records, etc.. In the admin view of this page, every form and section is visible, in contrast to the the non-admin view where only specific sections would be accessible depending on the person logged in.</p>
-					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, HTML5, MySQL, Wordpress, JavaScript, jQuery, AJAX</p>
+					<p><span class='descriptor'>Tools: </span> PHP 5.4, CSS3, Foundation 6, HTML5, MySQL, Wordpress, JavaScript, AJAX</p>
 				</div>
 			</div>
 			
+			<div class='info-block'>
+				<a href='https://github.com/KyleBonar' target='_blank'><span class='header'>GitHub</span>
+				<svg vector-effect="non-scaling-stroke" xmlns="http://www.w3.org/2000/svg" width="29" height="32px" viewBox="0 0 1289 1181">
+						<rect fill="none" x="-1" y="-1" width="1289" height="1181"/>
+						<path d="m1010.4 714.2l0 244q0 91.3-60.5 156.1t-145.7 64.8l-595.9 0q-85.2 0-145.7-64.8t-60.5-156.1l0-638.1q0-91.3 60.5-156.1t145.7-64.8l504.2 0q10 0 16.5 6.9t6.4 17.6l0 49.1q0 10.7-6.4 17.6t-16.5 6.9l-504.2 0q-47.3 0-80.9 36t-33.7 86.7l0 638.1q0 50.6 33.7 86.7t80.9 36l595.9 0q47.3 0 80.9-36t33.7-86.7l0-245.4q0-10.7 6.4-17.6t16.5-6.9l45.8 0q10 0 16.5 6.9t6.4 17.9zm275-662.9l0 392.7q0 19.9-13.6 34.5t-32.2 14.6 -32.2-14.6l-126.1-135 -467 500q-7.2 7.7-16.5 7.7t-16.5-7.7l-81.6-87.4q-7.2-7.7-7.2-17.6t7.2-17.6l467-500 -126.1-135q-13.6-14.6-13.6-34.5t13.6-34.5 32.2-14.6l366.7 0q18.6 0 32.2 14.6t13.6 34.5z" fill="#000"/>
+					</svg>
+				</a>
+				<div class='details'>
+					<p>You can find some of the public code I've written at <a href='https://github.com/KyleBonar' target="_blank">https://github.com/KyleBonar</a>.</p>
+				</div>
+			</div>
 
 		</div>
 		<div id='contact-block' class='page'>
 
 			<div class='info-block'>
-				<span class='header'>GitHub </span>
-				<div class='details'>
-					<p>You can find some of the public code I've written at <a href='https://github.com/KyleBonar'>https://github.com/KyleBonar</a>.</p>
-				</div>
-			</div>
-
-			<div class='info-block'>
 				<span class='header'>Email</span>
 				<div class='details'>
-					<p>Email me at <a href='mailto:kylewbonar@gmail.com'>kylewbonar@gmail.com</a>. I'll be sure to get back to you as soon as I can.</p>
+					<p>Email me at <a href='mailto:kylewbonar@gmail.com'>kylewbonar@gmail.com</a>. I'll be sure to get back to you promptly.</p>
 				</div>
 			</div>
 			
@@ -79,7 +84,7 @@
 			<div class='info-block'>
 				<span class='header'>R&eacute;sum&eacute;</span>
 				<div class='details'>
-					<p>You can find my r&eacute;sum&eacute; <a href='http://kylebonar.com/images/Bonar_Kyle_2016.pdf'> here</a>.</p>
+					<p>You can view my r&eacute;sum&eacute; <a href='http://kylebonar.com/images/Bonar_Kyle_2016.pdf' target='_blank'> here</a>.</p>
 				</div>
 			</div>
 		</div>
